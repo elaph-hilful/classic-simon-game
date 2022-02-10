1df2138a70e95c86a7fc0338e493e5fdbe19a4e5
hilful25 <hilful25@gmail.com>
2022-02-10 22:29:21 +0600

    fixed issue regarding btn click

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 
 var buttonColours = ["red","blue","green","yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 // alert("This is the game.js page!!!");
 function nextSequence(){
     var randomNumber = Math.floor(Math.random()*4);
@@ -8,11 +9,25 @@ function nextSequence(){
     var randomChosenColour = buttonColours[randomNumber];
     // console.log(randomChosenColour);
     
-    gamePattern.push(randomChosenColour)
+    gamePattern.push(randomChosenColour);
     $("#"+ randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    var sound = () =>{
-        new Audio("sounds/" + randomChosenColour+ ".mp3");
-    }
+    
+    // var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    // audio.play();
+    
+    playSound(randomChosenColour);
+    
 }
+$(".btn").click(function() {
 
-nextSequence();
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+  
+    //1. In the same way we played sound in nextSequence() , when a user clicks on a button, the corresponding sound should be played.
+    playSound(userChosenColour);
+  
+  });
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
